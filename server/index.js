3cb316c096de9a467f0b097f497cc680a380668c
Ryan Shaw <ryan.shaw@min.v>
2014-10-18 16:29:10 +0000

    Added console input

--- server/index.js
+++ server/index.js
@@ -6,6 +6,13 @@ var sp = new SerialPort('/dev/ttyACM0', {
 	baudrate: 115200
 });
 
+var sys = require('sys');
+
+var stdin = process.openStdin();
+stdin.addListener('data', function(data){
+		
+});
+
 sp.on('open', function(){
 	console.log('Serial port to Arduino opened');
 	sp.on('data', function(data){
