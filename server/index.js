1a7dcf0fad2df8fab9843558be61711ed119366a
Ryan <ryan.shaw@min.vc>
2014-10-19 19:49:44 +0100

    Little fix

--- server/index.js
+++ server/index.js
@@ -15,12 +15,7 @@ var sp;
 var path;
 serialport.list(function(err, ports){
 	ports.forEach(function(port){
-		console.log(port.comName);
-		console.log(port.pnpId);
-		console.log(port.manufacturer);
-		if(port.manufacturer.indexOf('Arduino') != -1){
-			path = port.comName;
-		}
+		path = port.comName;
 	});
 	sp = new SerialPort(path, {
 		parser: serialport.parsers.readline('\n'),
